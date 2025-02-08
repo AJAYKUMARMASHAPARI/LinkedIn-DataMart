695a55e4c9d499b6614732cf16716d47dc0e6f95
ajaykumarmashapari <donard803@gmail.com>
2025-02-08 17:33:54 -0600

    Initial commit - LinkedIn DataMart project

---
+++ dump_Group_E.sql
@@ -0,0 +1,14 @@
+SHOW TABLES;
+
+SELECT * FROM demographic;
+
+SELECT * FROM languages;
+SELECT * FROM certifications;
+SELECT * FROM connections;
+SELECT * FROM education;
+SELECT * FROM experiences;
+SELECT * FROM languages;
+SELECT * FROM patent;
+SELECT * FROM projects;
+SELECT * FROM publications;
+SELECT * FROM volunteer;
